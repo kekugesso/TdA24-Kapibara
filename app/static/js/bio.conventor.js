a90c1cbabbe51f66fd568ad327c5029711deaa20
Vojtěch <149375622+VSladek@users.noreply.github.com>
2023-11-11 17:35:31 +0000

    Lecturer final + moblie suport

---
+++ app/static/js/bio.conventor.js
@@ -0,0 +1,6 @@
+/*!
+  * TDA
+  * Converts html bio import into html
+  */
+document.getElementById("bio").innerHTML = document.getElementById("bio").textContent;
+document.getElementById("biomobile").innerHTML = document.getElementById("biomobile").textContent;
