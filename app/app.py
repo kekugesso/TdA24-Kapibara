e10dae63b9d438a470265307dbe93931ed1437b6
kekugesso <vitalymir16@gmail.com>
2024-01-03 17:51:37 +0100

    Add Database

--- app/app.py
+++ app/app.py
@@ -1,14 +1,14 @@
 import os
 import json
+import uuid
 from flask import *
-# Flask, request, url_for, redirect, render_template
-from . import db
+from models import db, Lecturer, Tags, Contacts, lecture_tag
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-
-app.config.from_mapping(
-    DATABASE=os.path.join(app.instance_path, 'tourdeflask.sqlite'),
-)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db.init_app(app)
+migrate = Migrate(app, db)
 
 # ensure the instance folder exists
 try:
@@ -16,31 +16,53 @@ try:
 except OSError:
     pass
 
-db.init_app(app)
-
+@app.before_request
+def before_request():
+    db.create_all()
 
 @app.route('/')  # title page
 def title():
     return render_template('index.html')
 
-
 @app.route('/api')  # api
 def json_api():
-    return json.dumps('{ "secret": "The cake is a lie"}')
-
+    return json.dumps({'secret': 'The cake is a lie'})
 
 @app.route('/lecturer')  # lecturer
 def lecturer_static():
-    data = json.load(open('app/static/json/lecturer.json', 'r'))
-    return render_template('lecturer.html', data=data)
+    lecturers_data = Lecturer.query.first()
+    return render_template('lecturer.html', data=lecturers_data)
 
+@app.route('/lecturer/<int:lecturer_id>')  # Lecturer - specific
+def lecturer_specific(lecturer_id):
+    lecturer_data = Lecturer.query.get(lecturer_id)
+    return render_template('lecturer.html', lecturer=lecturer_data)
 
+@app.route('/test', methods=['POST', 'GET'])
+def test():
+    if request.method == "POST":
+        new_uuid = str(uuid.uuid4())
+        try:
+            new_lecturer = Lecturer(
+                uuid = new_uuid,
+                title_before=request.form['title_before'],
+                first_name=request.form['first_name'],
+                middle_name=request.form['middle_name'],
+                last_name=request.form['last_name'],
+                title_after=request.form['title_after'],
+                picture_url=request.form['picture_url'],
+                location=request.form['location'],
+                claim=request.form['claim'],
+                bio=request.form['bio'],
+                price_per_hour=int(request.form['price_per_hour'])
+            )
+            db.session.add(new_lecturer)
+            db.session.commit()
+        except Exception as e:
+            # Обработка исключения при возникновении ошибки
+            return f"Error: {str(e)}"
 
-@app.route('/lecturer/<uuid>')  # Lecturer - spesific
-def lecturer_specific(uuid):
-    data = json.load(open('app/static/json/lecturer.json', 'r'))
-    return render_template('lecturer.html', lecturer_uuid=uuid, data=data)
-
+    return render_template('test.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ app/models.py
@@ -0,0 +1,58 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Lecturer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(db.Text, nullable = False)
+    title_before = db.Column(db.Text, nullable=False)
+    first_name = db.Column(db.Text, nullable=False)
+    middle_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    title_after = db.Column(db.Text, nullable=False)
+    picture_url = db.Column(db.Text, nullable=False)
+    location = db.Column(db.Text, nullable=False)
+    claim = db.Column(db.Text, nullable=False)
+    bio = db.Column(db.Text, nullable=False)
+    price_per_hour = db.Column(db.Integer, nullable=False)
+    
+    tags = db.relationship('Tag', secondary='lecture_tag', back_populates='lecturers')
+
+    contacts_id = db.Column(db.Integer, db.ForeignKey('contacts.id'), nullable=True)
+
+    contacts = db.relationship('Contacts', back_populates='lecturer', uselist=False)
+
+    def __repr__(self):
+        return f'<Lecturer {self.id}>'
+
+class Tags(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(db.Integer, nullable = False)
+    name = db.Column(db.Text, nullable=False)
+    lecturers = db.relationship('Lecturer', secondary='lecture_tag', back_populates='tags')
+
+    def __repr__(self):
+        return f'<Tags {self.id}>'
+
+class Contacts(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    phone1 = db.Column(db.Text, nullable = True)
+    phone2 = db.Column(db.Text, nullable = True)
+    phone3 = db.Column(db.Text, nullable = True)
+    phone4 = db.Column(db.Text, nullable = True)
+    phone5 = db.Column(db.Text, nullable = True)
+    email1 = db.Column(db.Text, nullable = True)
+    email2 = db.Column(db.Text, nullable = True)
+    email3 = db.Column(db.Text, nullable = True)
+    email4 = db.Column(db.Text, nullable = True)
+    email5 = db.Column(db.Text, nullable = True)
+
+    lecturer = db.relationship('Lecturer', back_populates='contacts', uselist=False)
+
+    def __repr__(self):
+        return f'<Contacts {self.id}>'
+
+lecture_tag = db.Table('lecture_tag',
+                    db.Column('lecture_id', db.Integer, db.ForeignKey('lecturer.id')),
+                    db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'))
+                    )
