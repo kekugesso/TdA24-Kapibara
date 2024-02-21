c16b97055ac4c8daea2b0550ca159c7fc8c2545f
Vojtěch <149375622+VSladek@users.noreply.github.com>
2024-02-21 22:31:36 +0100

    modal fixed - downgraded bootrap.js (5.3.x >> 4.6.2) only for calendar

--- app/static/js/calendar/app.js
+++ app/static/js/calendar/app.js
@@ -1,93 +1,54 @@
-(function($) {
-
-	"use strict";
-
-	var options = {
-		events_source: {
-            "id": 1,
-            "title": "Event 1",
-            "url": "http://example.com",
-            "class": "event-important",
-            "start": 1708446760885, // Milliseconds
-            "end": 1708449060885 // Milliseconds
-        },
-		view: 'month',
-		tmpl_path: 'static/js/calendar/tmpls/tmpls/',
-		tmpl_cache: false,
-		onAfterEventsLoad: function(events) {
-			if(!events) {
-				return;
-			}
-			var list = $('#eventlist');
-			list.html('');
-
-			$.each(events, function(key, val) {
-				$(document.createElement('li'))
-					.html('<a href="' + val.url + '">' + val.title + '</a>')
-					.appendTo(list);
-			});
-		},
-		onAfterViewLoad: function(view) {
-			$('.page-header h3').text(this.getTitle());
-			$('.btn-group button').removeClass('active');
-			$('button[data-calendar-view="' + view + '"]').addClass('active');
-		},
-		classes: {
-			months: {
-				general: 'label'
-			}
+var options = {
+	tmpl_path: "../static/js/calendar/tmpls/",
+	language: 'cs-CZ',
+	first_day: 1,
+	view: 'month',
+	weekbox: true,
+	display_week_numbers: true,
+	modal: '#events-modal',
+	modal_type: 'iframe',
+	modal_title: function(event) { return event.title },
+	time_split: 60,
+	events_source: events_data,
+	onAfterEventsLoad: function(events) {
+		if(!events) { return; }
+		var list = $('#eventlist');
+		list.html('');
+		$.each(events, function(key, val) {
+			$(document.createElement('li'))
+				.html('<a href="' + val.url + '">' + val.title + '</a>')
+				.appendTo(list);
+		});
+	},
+	onAfterViewLoad: function(view) {
+		$('.page-header h3').text(this.getTitle());
+		$('.btn-group button').removeClass('active');
+		$('button[data-calendar-view="' + view + '"]').addClass('active');
+	},
+	classes: {
+		months: {
+			general: 'label'
 		}
-	};
+	}
+}
+var calendarElement = $('#calendar');
+var calendar = calendarElement.calendar(options);
 
-	var calendar = $('#calendar').calendar(options);
+$('.btn-group button[data-calendar-nav]').each(function() {
+	var $this = $(this);
+	$this.click(function() {
+		calendar.navigate($this.data('calendar-nav'));
+	});
+});
 
-	$('.btn-group button[data-calendar-nav]').each(function() {
-		var $this = $(this);
-		$this.click(function() {
-			calendar.navigate($this.data('calendar-nav'));
-		});
+$('.btn-group button[data-calendar-view]').each(function() {
+	var $this = $(this);
+	$this.click(function() {
+		calendar.view($this.data('calendar-view'));
 	});
+});
 
-	$('.btn-group button[data-calendar-view]').each(function() {
-		var $this = $(this);
-		$this.click(function() {
-			calendar.view($this.data('calendar-view'));
-		});
-	});
-
-	$('#first_day').change(function(){
-		var value = $(this).val();
-		value = value.length ? parseInt(value) : null;
-		calendar.setOptions({first_day: value});
-		calendar.view();
-	});
-
-	$('#language').change(function(){
-		calendar.setLanguage($(this).val());
-		calendar.view();
-	});
-
-	$('#events-in-modal').change(function(){
-		var val = $(this).is(':checked') ? $(this).val() : null;
-		calendar.setOptions({modal: val});
-	});
-	$('#format-12-hours').change(function(){
-		var val = $(this).is(':checked') ? true : false;
-		calendar.setOptions({format12: val});
-		calendar.view();
-	});
-	$('#show_wbn').change(function(){
-		var val = $(this).is(':checked') ? true : false;
-		calendar.setOptions({display_week_numbers: val});
-		calendar.view();
-	});
-	$('#show_wb').change(function(){
-		var val = $(this).is(':checked') ? true : false;
-		calendar.setOptions({weekbox: val});
-		calendar.view();
-	});
-	$('#events-modal .modal-header, #events-modal .modal-footer').click(function(e){
-		//e.preventDefault();
-		//e.stopPropagation();
-	});
-}(jQuery));
+$('#events-modal .modal-header, #events-modal .modal-footer').click(function(e){
+	//e.preventDefault();
+	//e.stopPropagation();
+});
