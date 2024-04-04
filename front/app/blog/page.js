1ec244a9db140ffe6605626e8d595298fd11aae1
Vojtěch <149375622+VSladek@users.noreply.github.com>
2024-04-04 11:19:25 +0000

    some small changes

---
+++ front/app/blog/page.js
@@ -0,0 +1,6 @@
+export default function Blog() {
+    return (
+      <h1>Hello my blog!</h1>
+    );
+  }
+  
--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const API_PORT = process.env.API_PORT
+const IP = process.env.IP
+const nextConfig = {	
+    async rewrites() {
+    return [
+        {
+            source: '/api/:path*',
+            destination: `http://${IP}:${API_PORT}/api/:path*`,
+        },
+    ]
+},};
 
 export default nextConfig;
