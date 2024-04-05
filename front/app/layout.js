94047680d4d1f2cc9db2043c7595798581f94144
Vojtěch <149375622+VSladek@users.noreply.github.com>
2024-04-05 10:24:49 +0000

    added some layout

--- front/app/layout.js
+++ front/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Nav from "./nav";
+import Footer from "./footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={'${inter.className} flex flex-col h-screen justify-between'}>
+        <Nav />
+        <main className="mb-auto">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ front/app/nav.js
@@ -0,0 +1,8 @@
+export default function Nav() {
+    return (
+      <header className="grid min-h-10 place-items-center bg-gray-400 px-6 py-24 sm:py-32 lg:px-8">
+        <h1>Hello my nav!</h1>
+      </header>
+    );
+}
+  
