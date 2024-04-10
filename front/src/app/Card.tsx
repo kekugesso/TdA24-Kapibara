8082ec1f4487e797b1d8a430fabc2b1063bae856
Vojtěch <149375622+VSladek@users.noreply.github.com>
2024-04-10 07:32:02 +0000

    some changes to card

--- front/src/app/Card.tsx
+++ front/src/app/Card.tsx
@@ -3,17 +3,39 @@ import { Lecturer_Card } from "./lecturer";
 export default function Card({ lecturer }: {lecturer: Lecturer_Card}) {
 
     return (
-        <section className="flex min-h-screen bg-white dark:bg-jet text-black dark:text-white flex-col items-center justify-between p-24 border-2 border-jet">
-            <h1>{lecturer.fullName}</h1>
-            <h2>{lecturer.location}</h2>
-            <img src={lecturer.picture_url} alt="Lecturer" />
-            <p>{lecturer.claim}</p>
-            <p>{lecturer.price_per_hour}</p>
-            <div className="flex border-2 border-jet">
-                {lecturer.tags.map((tag) => (
-                    <p key={tag.uuid}>{tag.name}</p>
-                ))}
+        <figure className="flex bg-blue dark:bg-jet rounded-xl p-8 md:p-0 dark:bg-slate-800 border-2 border-jet dark:border-white">
+            <img className="w-24 h-24 md:w-48 md:h-auto rounded-none mx-auto" src={lecturer.picture_url} alt={lecturer.fullName}  />
+            <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+                <p>{lecturer.fullName}</p>
+                <p>{lecturer.location}</p>
+                <p>{lecturer.claim}</p>
+                <p>{lecturer.price_per_hour}</p>
+                <div className="flex">
+                    {lecturer.tags.map((tag) => (
+                        <span key={tag.uuid} className="bg-yellow rounded-full mx-1 px-2 justify-center text-center text-nowrap text-jet">{tag.name}</span>
+                    ))}
+                </div>
             </div>
-        </section>
-    );
+        </figure>
+        //<figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+        //<img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512">
+        //<div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+            //<blockquote>
+            //<p class="text-lg font-medium">
+                //“Tailwind CSS is the only framework that I've seen scale
+                //on large teams. It’s easy to customize, adapts to any design,
+                //and the build size is tiny.”
+            //</p>
+            //</blockquote>
+            //<figcaption class="font-medium">
+            //<div class="text-sky-500 dark:text-sky-400">
+                //Sarah Dayan
+            //</div>
+            //<div class="text-slate-700 dark:text-slate-500">
+                //Staff Engineer, Algolia
+            //</div>
+            //</figcaption>
+        //</div>
+        //</figure>
+     );
 }
