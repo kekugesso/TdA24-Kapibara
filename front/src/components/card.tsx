8d624c29d41d912507a94f784690628b0eb76376
Vojtěch <149375622+VSladek@users.noreply.github.com>
2024-04-17 22:09:52 +0200

    added hover transformation, renamed fetcher

--- front/src/components/card.tsx
+++ front/src/components/card.tsx
@@ -12,10 +12,10 @@ export default function Card({ lecturer, index }: { lecturer: Lecturer_Card, ind
   const isVisible = IsVisible(ref);
 
   return (
-    <Link href={"/lecturer/" + lecturer.uuid}>
-      <div ref={ref} className={"grid grid-cols-1 items-center h-96 bg-jet/15 dark:bg-jet rounded-xl p-8 border-2 border-jet dark:border-white overflow-hidden" + (isVisible ? " opacity-100 animate-fade-up animate-ease-in-out animate-alternate " + (isOdd(index) ? "animate-delay-[100ms]" : "") : " opacity-0")}>
-        <img className="w-36 h-36 rounded-full mx-auto" src={lecturer.picture_url} alt={"picture of " + lecturer.fullName} />
+    <Link href={"/lecturer/" + lecturer.uuid} className="transition ease-in-out delay-100 hover:scale-105 duration-300">
+      <div ref={ref} className={"grid grid-cols-1 h-96 bg-jet/15 dark:bg-jet rounded-xl p-8 border-2 border-jet dark:border-white overflow-hidden" + (isVisible ? " opacity-100 animate-fade-up animate-ease-in-out animate-alternate " + (isOdd(index) ? "animate-delay-[100ms]" : "") : " opacity-0")}>
         <div className="p-2 text-center space-y-2">
+          <img className="w-36 h-36 rounded-full mx-auto items-center align-middle border-2 border-white" src={lecturer.picture_url} alt={"picture of " + lecturer.fullName} />
           <p><strong>{lecturer.fullName}</strong></p>
           <p>{lecturer.claim}</p>
           <div className="grid grid-cols-2 gap-10">
@@ -26,11 +26,11 @@ export default function Card({ lecturer, index }: { lecturer: Lecturer_Card, ind
               {lecturer.price_per_hour} kč
             </span>
           </div>
-          <div className="flex flex-wrap max-w-screen-sm text-sm text-center text-black/55 dark:text-white/25">
-            {lecturer.tags.map((tag) => (
-              <a className="mx-1" href="">#{tag.name}</a>
-            ))}
-          </div>
+        </div>
+        <div className="flex flex-wrap max-w-screen-sm text-sm text-center text-black/55 dark:text-white/25">
+          {lecturer.tags.map((tag) => (
+            <Link href={"?tag=" + tag.uuid}><div className="mx-1 hover:animate-pulse hover:animate-once">#{tag.name}</div></Link>
+          ))}
         </div>
       </div>
     </Link>
