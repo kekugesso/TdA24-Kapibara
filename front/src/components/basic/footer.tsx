0508aab4af863806570da8c999e8f2eaa0cff790
Vojtěch <149375622+VSladek@users.noreply.github.com>
2024-06-01 15:00:34 +0200

    fix: missing fix leyout decomposition

--- front/src/components/basic/footer.tsx
+++ front/src/components/basic/footer.tsx
@@ -4,7 +4,7 @@ import dayjs from "dayjs";
 
 export default function Footer({ bgColor }: { bgColor: string }) {
   return (
-    <footer className={bgColor + " text-white grid min-h-10 place-items-center px-6 py-6 lg:px-8"}>
+    <footer className={`${bgColor} text-white grid min-h-10 place-items-center px-6 py-6 lg:px-8 justify-self-end`}>
       <h1>© {dayjs().year().toString()} Teacher Digital Agency</h1>
     </footer>
   );
