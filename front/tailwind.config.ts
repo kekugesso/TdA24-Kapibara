b60b626cf7e08f7a3a78d641ed8cd2d369d52202
Vojtěch <149375622+VSladek@users.noreply.github.com>
2024-06-01 15:01:36 +0200

    fix: fixed colors outside of extended

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -12,15 +12,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        black: "#000000",
+        white: "#ffffff",
+        jet: "#333333",
+        dark_blue: "#00384D",
+        blue: "#74C7D3",
+        yellow: "#FECB2E",
+      }
     },
-    colors: {
-      black: "#000000",
-      white: "#ffffff",
-      jet: "#333333",
-      dark_blue: "#00384D",
-      blue: "#74C7D3",
-      yellow: "#FECB2E",
-    }
   },
   plugins: [
     require('tailwindcss-animated')
