a0c098e005f32552dc91f19d4dc95c223a242dab
Vojtěch <149375622+VSladek@users.noreply.github.com>
2024-06-07 23:14:24 +0200

    feat: added start script for backend

--- back/start-back.sh
+++ back/start-back.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
+python3 -m venv venv
+source venv/bin/activate
 # install requirements
 pip install -r requirements.txt
 
-# init db
-python3 -m flask --app app/app.py init-db
-
-# run 
-if [ "$1" = "prod" ]; then
-	python3 -m flask --app app/app.py run --host=0.0.0.0 --port=2197
-else
-	python3 -m flask --app app/app.py run --debug --port=2197
-fi
+python3 app_django/manage.py runserver 127.0.0.1:2179
